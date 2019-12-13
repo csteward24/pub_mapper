271fbf25559a7cca0be52cedb559e9ef04e491ac
mcolacic <58610276+mcolacic@users.noreply.github.com>
2019-12-13 18:38:51 -0500

    Add files via upload

---
+++ about_us.php
@@ -0,0 +1,46 @@
+<head>
+<link rel = "stylesheet"   href ="./styles.css"/>
+
+</head>
+<div>
+<?php
+	include 'header.php';
+?>
+</div>
+<div class = "mainContent">
+<div class = "aboutUsTitle">
+
+	<h3> ABOUT US </h3>
+</div>
+
+<p>
+<div class = "drinkPhoto">
+<img src="./photos/drink.png" alt="Bar">
+</div>
+<div class = "aboutUsText">
+Are you in the South Jersey and Philadelphia area? We got just what you need! If you are in town and need something to do, we thought it would be a good idea to inform everyone on the bars they could visit nearby. This is a good area to be visiting since you ahve Philly bars as well as South Jersey bars. The map we show you is for the South Jersey/Philadelphia area. We include bars like Landmark Americana which  has good deals, tv's, and happy hour, as well as the Voltage Lounge in Phildaelphia where you get great drinks and a music perfromance!
+
+<br>
+
+<br>
+
+With this map, you are able to choose which bars you want to go to, then we generate the  map for you and the path from bar to bar.
+
+<br>
+
+<br>
+
+Once you exit our website, you will successfully have a whole map of where your night will begin and end! 
+
+
+
+
+</p>
+</div>
+</div>
+<div>
+<?php
+	include 'footer.php';
+?>
+
+</div>

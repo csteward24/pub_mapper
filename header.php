4103fe7943b186ba6b806f4a9607ce1f73051fe5
mcolacic <58610276+mcolacic@users.noreply.github.com>
2019-12-16 20:56:32 -0500

    Update header.php

--- header.php
+++ header.php
@@ -9,16 +9,16 @@
 </div>
 <div class = "headerContent">
         <div class = "logo">
-        <a href = "test.php"><img src = "./photos/pub_crawl.png" alt = "Home"></a>
+        <a href = "test.php"><img src = "./pub_crawl.png" alt = "Home"></a>
         </div>
         <div class = "home">
-        <a href = "test.php"><img src = "./photos/home.png" alt = "Home"></a>
+        <a href = "test.php"><img src = "./home.png" alt = "Home"></a>
 </div>
         <div class = "contact_us">
-        <a href = "contact_us.php"><img src = "./photos/contact_us.png" alt = "Contact Us"></a>
+        <a href = "contact_us.php"><img src = "./contact_us.png" alt = "Contact Us"></a>
         </div>
         <div class = "about_us">
-        <a href = "about_us.php"><img src = "./photos/about_us.png" alt = "About Us"></a>
+        <a href = "about_us.php"><img src = "./about_us.png" alt = "About Us"></a>
         </div>
 
 
